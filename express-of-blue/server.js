c3111bf5b778656074ac8aa38b1d3d973d90f81c
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-11 18:19:14 -0700

    review express basic

---
+++ express-of-blue/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+var server= express();
+server.use('/a.html', function(req,res) {
+  res.send('abc');
+  res.end();
+});
+server.use('/b.html', function(req,res) {
+    res.send('123');
+    res.end();
+  });
+server.listen(8080);
