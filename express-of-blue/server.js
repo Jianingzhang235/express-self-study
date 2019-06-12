0c9f705eb6577d91b2a9a9877c7e48c58eb10521
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-11 19:39:52 -0700

    undestand get, post and use

--- express-of-blue/server.js
+++ express-of-blue/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 var server= express();
 server.use('/a.html', function(req,res) {
-  res.send('abc');
+  res.send({a: 12, b: 5});
   res.end();
 });
 server.use('/b.html', function(req,res) {
     res.send('123');
     res.end();
   });
+  server.use('/', function() {
+    console.log('use req')
+  })
 server.listen(8080);
